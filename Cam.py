f2825800d619abc080905a60e1be4f5f48d00d6c
Mason Fang <masonfang@Masons-MacBook-Air.local>
2025-09-26 10:23:19 -0400

    Add face detection script

--- Cam.py
+++ Cam.py
@@ -1,10 +1,43 @@
 import cv2
 
-stream = cv2.VideoCapture(0)
+#feature detecter
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_smile.xml")
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+
+def detect_features(frame):
+    #if no smile or eyes program still runs
+    smiles = []
+    eyes = []
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #turns gray for faster processing
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) #find faces within frame
+    for (x, y, w, h) in faces:
+        frame = cv2.rectangle(frame, (x, y), (x+w, y+h+100), color = (0, 255, 0), thickness = 5) #draws box around face
+
+        face = frame[y : y+h, x : x+w] #extract face region
+        gray_face = gray[y : y+h, x : x+w]
+
+        smiles = smile_cascade.detectMultiScale(gray_face, 2.5, minNeighbors=14) #finds smile
+        eyes = eye_cascade.detectMultiScale(gray_face, 2.5, minNeighbors=10) 
+
+    for (xp, yp, wp, hp) in smiles:
+        face = cv2.rectangle(face, (xp, yp), (xp+wp, yp+hp), color = (0, 0, 255), thickness = 5) #draws rectangle around smile
+        
+        
+    for (xp, yp, wp, hp) in eyes:
+        face = cv2.rectangle(face, (xp, yp), (xp+wp, yp+hp), color = (255, 0, 0), thickness = 5)
+    
+    return frame
+
+
+stream = cv2.VideoCapture(0) #loads webcam
+
 
 if not stream.isOpened():
     print("No stream")
     exit()
+
 
 while(True):
     ret, frame = stream.read()
@@ -12,8 +45,9 @@ while(True):
         print("no more stream")
         break
 
+    frame = detect_features(frame)
     cv2.imshow("webcam", frame)
-    if cv2.waitKey(1) == ord("q"):
+    if cv2.waitKey(1) == ord("q"): # exits webcam
         break
 
 stream.release()
